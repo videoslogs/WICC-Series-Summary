e2e60ef0a4a5fc17af0e30cf6d5e7797920b2fcf
videoslogs <147054753+videoslogs@users.noreply.github.com>
2026-01-21 16:01:05 +0000

    Create supabaseClient.ts

---
+++ supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Vercel will provide these from your Environment Variables
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
